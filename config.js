e80a0dd6b37180a13d081079e5aecfe249a9e385
Mikael Strengell <Mikael.Strengell@balticyachts.fi>
2026-09-23 11:21:38 +0300

    Fix Firebase authDomain for redirect login

--- config.js
+++ config.js
@@ -2,5 +2,5 @@
 window.LOGG_CONFIG = {
   API_BASE: "https://logg-speech-273392986972.europe-west1.run.app",
   BUILD: "0.12.0",
-  FIREBASE: { apiKey: "AIzaSyAQnyI0xRXD00asVl7PvFkDFwLt18_DZpU", authDomain: "nimble-skein-lfs6l.firebaseapp.com", projectId: "nimble-skein-lfs6l" }
+  FIREBASE: { apiKey: "AIzaSyAQnyI0xRXD00asVl7PvFkDFwLt18_DZpU", authDomain: authDomain: "nimble-skein-lfs6l.web.app", projectId: "nimble-skein-lfs6l" }
 };
